00d9cd2736877600ae8aa010af43694ca1fdf3ba
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-15 11:45:44 -0800

    Change task status to completed.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,9 @@ function Task (description) {
   this.description = description;
   this.isComplete = false;
 }
+
+//When Task.prototype.completeTask is called in UIL it should also change something about the appearance -- Add a checkmark in front of it? Add "Completed: " in front of it?
+Task.prototype.completeTask = function() { 
+  this.isComplete = true;
+  //Does it need to return true? Will that return value need to be utilized?
+}
