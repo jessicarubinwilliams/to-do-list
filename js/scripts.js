50ea90e051d2bece1d4646e63d1d0b70fdf4b007
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-15 11:55:57 -0800

    Change task status to incomplete.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,14 @@ function Task (description) {
   this.isComplete = false;
 }
 
-//When Task.prototype.completeTask is called in UIL it should also change something about the appearance -- Add a checkmark in front of it? Add "Completed: " in front of it?
+//When Task.prototype.completeTask is called in UIL it should also change something about the appearance -- Add a checkmark in front of it? Add "Completed: " in front of it? $.addclass()?
 Task.prototype.completeTask = function() { 
   this.isComplete = true;
+  //Does it need to return true? Will that return value need to be utilized?
+}
+
+//When Task.prototype.undoCompleteTask is called in UIL it should also change something about the appearance -- Remove whatever .completeTask() added $.removeclass()?
+Task.prototype.undoCompleteTask = function() {
+  this.isComplete = false;
   //Does it need to return true? Will that return value need to be utilized?
 }
